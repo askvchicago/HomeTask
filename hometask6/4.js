5d1bf910d063e64bcf3e1b07cb8923e3c54cac4f
Anton Skvarcheuski <i@askvchicago.ru>
2024-02-08 04:12:43 +0300

    Home task 4

---
+++ hometask6/4.js
@@ -0,0 +1,15 @@
+// Задание 4
+
+const arr = [300, 3, 67, 15, 28, 109, 56];
+
+let maxNumber = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxNumber) {
+        maxNumber = arr[i];
+    }
+}
+
+console.log('Максимальное значение:', maxNumber);
+
+// done
